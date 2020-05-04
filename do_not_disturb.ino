f0938ee69595e44c7a9edf407ac7d7617b41cb3a
Jake Brehm <jake.m.brehm@gmail.com>
2020-05-03 21:08:45 -0400

    Added code for the buttons.

--- do_not_disturb.ino
+++ do_not_disturb.ino
@@ -2,6 +2,10 @@ int greenLED = 2; // the pin the LED is connected to
 int yellowLED = 3;
 int redLED = 4;
 
+int greenButton = 6;
+int yellowButton = 7;
+int redButton = 8;
+
 void green();
 void yellow();
 void red();
@@ -9,17 +13,34 @@ void red();
 void (*functionPointers[])() = {green, yellow, red};
 
 void setup() {
+  Serial.begin(9600);
+  
   pinMode(greenLED, OUTPUT); // Declare the LED as an output
   pinMode(yellowLED, OUTPUT);
   pinMode(redLED, OUTPUT);
+
+  pinMode(greenButton, INPUT);
 
   off();
 }
 
 void loop() {
-  int index = random(0, 3);
-  functionPointers[index]();
-  delay(1000);
+//  int index = random(0, 3);
+//  functionPointers[index]();
+//  delay(250);
+
+  if ( digitalRead(greenButton) == HIGH ) {
+    green();
+  } else if ( digitalRead(yellowButton) == HIGH ) {
+    yellow();
+  } else if ( digitalRead(redButton) == HIGH ) {
+    red();
+  }
+
+  Serial.println(digitalRead(greenButton));
+
+  delay(50);
+
 }
 
 void off() {
